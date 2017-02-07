1404802f1a24054a9df48ad0cb89dd11ec280a2a
James <jwin4740@gmail.com>
2017-02-07 00:03:46 -0500

    firebase

--- assets/javascript/starter.js
+++ assets/javascript/starter.js
@@ -1,36 +1,47 @@
 var boolBorder = false;
+var confessionArray = [];
 
-$(document).ready(function() {
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCYo9PGp7tCVZQ5zQ08GXau2Ic2AmcZw_E",
+    authDomain: "rpsgame-adee5.firebaseapp.com",
+    databaseURL: "https://rpsgame-adee5.firebaseio.com",
+    storageBucket: "rpsgame-adee5.appspot.com",
+    messagingSenderId: "919631435144"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+var dateRef = database.ref("/dates");
+
+function dateConstruct (day, confessionBool){
+    this.day = day,
+    this.confessionBool = confessionBool
+}
+
+
+console.log(confessionArray);
 
     for (var i = 1; i < 31; i++) {
         var monthBox = $("<div>");
         monthBox.addClass("datesblack");
+        monthBox.attr("data-value", i);
         monthBox.text(i);
         $(".test").append(monthBox);
     }
+   
+    dateRef.on("child_added", function(childSnapshot) {
+        var fireConfess = childSnapshot.val().confessionBool;
+        confessionArray.push(fireConfess);
+    });
 
-
+console.log(confessionArray);
     $(".datesblack").on("click", function() {
-        var location = $(this);
-        location.css({ "border": "solid 2px yellow" });
-        document.onkeyup = function(event) {
-            if (event.key === "r") {
-                location.css({ "border": "solid 4px red" });
-            }
-            if (event.key === "b") {
-                location.css({ "border": "solid 2px black" });
-            }
-        }
-
-
+        var date = $(this)[0];
+        console.log(date.attr());
+        $(this).css({ "border": "solid 3px red" });
+       
     });
-    $("#saveme").on("click", function(event) {
 
-        var testerval = $("#tester").val().trim();
-        localStorage.setItem("testvalue", testerval);
-        $("#runway").append(localStorage.getItem("testvalue"));
-    });
-    $("#runway").append(localStorage.getItem("testvalue"));
 
-});
